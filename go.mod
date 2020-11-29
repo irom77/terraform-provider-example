b230648a4f4cbd0acf9c2fc8bd24806815de6479
Irek Romaniuk <irekromaniuk@ireks-air.lan>
2020-11-28 20:31:08 -0500

    0.13 upgrade

--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
 module github.com/spaceapegames/terraform-provider-example
 
+go 1.15
+
 require (
 	github.com/davecgh/go-spew v1.1.1
 	github.com/gorilla/mux v1.6.2
--- main.tf
+++ main.tf
@@ -1,8 +1,21 @@
-provider "example" {
+/*provider "example" {
   address = "http://localhost"
   port    = "3001"
   token   = "superSecretToken"
+}*/
+
+terraform {
+  required_providers {
+    example = {
+      versions = ["0.1"]
+      source = "github.com/irom77/example"
+      address = "http://localhost"
+      port    = "3001"
+      token   = "superSecretToken"
+    }
+  }
 }
+
 
 resource "example_item" "test" {
   name = "this_is_an_item"
